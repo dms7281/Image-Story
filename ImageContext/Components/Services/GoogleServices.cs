c849f66da5e24da0a5e0d14ae3668279ca9f75d9
Cameron Schmidt <cameron@schmidty.com>
2024-09-01 17:49:16 -0400

    Fixed address correction with chat gpt. Search term is based on address now and not list of given search terms. Added web results to home page.

--- ImageContext/Components/Services/GoogleServices.cs
+++ ImageContext/Components/Services/GoogleServices.cs
@@ -12,54 +12,54 @@ public class GoogleServices(IHttpClientFactory httpClientFactory, IConfiguration
     private readonly string? _key = configuration["GoogleServicesKey"];
     private readonly string? _googleSearchEngine = configuration["GoogleSearchEngine"];
 
-    private async Task<List<string?>> GetSearchTerms(string jsonRequestString)
-    {
-        List<string> searchTerms = new List<string>();
-        List<string> excludedTypes = new List<string>{"street_number", "route", "intersection", "plus_code", "postal_code", "postal_code_suffix", "country"};
-        string concatenatedTerms = string.Empty;
-        using (JsonDocument doc = JsonDocument.Parse(jsonRequestString))
-        {
-            // Get the root element
-            JsonElement root = doc.RootElement;
-
-            JsonElement results = root.GetProperty("results");
-            
-            JsonElement addressComponents = results[0].GetProperty("address_components");
-
-            var addressCompArray = addressComponents.EnumerateArray().ToArray();
-            
-            //for (int i = addressCompArray.Length - 1; i >= 0; i--)
-            for (int i = 0; i < addressCompArray.Length; i++)
-            {
-                JsonElement types = addressComponents[i].GetProperty("types");
-            
-                foreach (var type in types.EnumerateArray())
-                {
-                    // Code currently excludes routes, work on implementing them with cities in the future
-                    if (excludedTypes.Contains(type.ToString())) break;
-                    
-                    var addressComp = addressComponents[i].GetProperty("long_name").ToString();
-
-                    if (searchTerms.Contains(addressComp)) break;
-                    
-                    searchTerms.Add(addressComp);
-
-                    // if (concatenatedTerms.Contains(addressComp)) break;
-                    //
-                    // concatenatedTerms = string.IsNullOrEmpty(concatenatedTerms) 
-                    //     ? $"\"{addressComp}\"" 
-                    //     : $"\"{addressComp}\" {concatenatedTerms}";
-                    //
-                    // // Add the new concatenated string to the search terms list
-                    // searchTerms.Insert(0, concatenatedTerms);
-                }
-            }
-            
-            Console.WriteLine("Google Geocoding API: Returned Search Terms");
-            
-            return searchTerms;
-        }
-    }
+    // private async Task<List<string?>> GetSearchTerms(string jsonRequestString)
+    // {
+    //     List<string> searchTerms = new List<string>();
+    //     List<string> excludedTypes = new List<string>{"street_number", "route", "intersection", "plus_code", "postal_code", "postal_code_suffix", "country"};
+    //     string concatenatedTerms = string.Empty;
+    //     using (JsonDocument doc = JsonDocument.Parse(jsonRequestString))
+    //     {
+    //         // Get the root element
+    //         JsonElement root = doc.RootElement;
+    //
+    //         JsonElement results = root.GetProperty("results");
+    //         
+    //         JsonElement addressComponents = results[0].GetProperty("address_components");
+    //
+    //         var addressCompArray = addressComponents.EnumerateArray().ToArray();
+    //         
+    //         //for (int i = addressCompArray.Length - 1; i >= 0; i--)
+    //         for (int i = 0; i < addressCompArray.Length; i++)
+    //         {
+    //             JsonElement types = addressComponents[i].GetProperty("types");
+    //         
+    //             foreach (var type in types.EnumerateArray())
+    //             {
+    //                 // Code currently excludes routes, work on implementing them with cities in the future
+    //                 if (excludedTypes.Contains(type.ToString())) break;
+    //                 
+    //                 var addressComp = addressComponents[i].GetProperty("long_name").ToString();
+    //
+    //                 if (searchTerms.Contains(addressComp)) break;
+    //                 
+    //                 searchTerms.Add(addressComp);
+    //
+    //                 // if (concatenatedTerms.Contains(addressComp)) break;
+    //                 //
+    //                 // concatenatedTerms = string.IsNullOrEmpty(concatenatedTerms) 
+    //                 //     ? $"\"{addressComp}\"" 
+    //                 //     : $"\"{addressComp}\" {concatenatedTerms}";
+    //                 //
+    //                 // // Add the new concatenated string to the search terms list
+    //                 // searchTerms.Insert(0, concatenatedTerms);
+    //             }
+    //         }
+    //         
+    //         Console.WriteLine("Google Geocoding API: Returned Search Terms");
+    //         
+    //         return searchTerms;
+    //     }
+    // }
 
     private async Task<List<string?>> GetFormatedAddresses(string jsonRequestString)
     {
@@ -79,7 +79,7 @@ public class GoogleServices(IHttpClientFactory httpClientFactory, IConfiguration
         }
     }
     
-    public async Task<(List<string?>, List<string?>)> GeocodingApi (string lat, string lng)
+    public async Task<List<string?>> GeocodingApi (string lat, string lng)
     {
         var httpClient = httpClientFactory.CreateClient("GoogleGeocoding");
 
@@ -88,17 +88,17 @@ public class GoogleServices(IHttpClientFactory httpClientFactory, IConfiguration
         if (request.IsSuccessStatusCode)
         {
             var requestJsonString = await request.Content.ReadAsStringAsync();
-            var searchTerms = await GetSearchTerms(requestJsonString);
+            //var searchTerms = await GetSearchTerms(requestJsonString);
             var formatedAddresses = await GetFormatedAddresses(requestJsonString);
             
             Console.WriteLine("Google Geocoding API: Request Successful");
 
-            return (searchTerms, formatedAddresses);
+            return formatedAddresses;
         }
         throw new Exception("Google Geocoding API: Request Failure");
     }
 
-    public async Task<List<WebResult>> SearchApi(List<string?> searchTerms, DateTime date)
+    public async Task<List<WebResult>> SearchApi(string searchPhrase, DateTime date)
     {
         string afterDate = date.AddDays(-1).ToString("yyyy-MM-dd");
         string beforeDate = date.AddDays(1).ToString("yyyy-MM-dd");
@@ -106,57 +106,54 @@ public class GoogleServices(IHttpClientFactory httpClientFactory, IConfiguration
         var httpClient = httpClientFactory.CreateClient("GoogleSearch");
 
         List<WebResult> webResults = new List<WebResult>();
-        // foreach (var term in searchTerms)
-        // {
             
-            var request = await httpClient.GetAsync($"v1?cx={_googleSearchEngine}&q={searchTerms[0]}+after:{afterDate}+before:{beforeDate}&key={_key}");
-    
-            if (request.IsSuccessStatusCode)
+        var request = await httpClient.GetAsync($"v1?cx={_googleSearchEngine}&q={searchPhrase}+after:{afterDate}+before:{beforeDate}&key={_key}");
+
+        if (request.IsSuccessStatusCode)
+        {
+            var jsonString = await request.Content.ReadAsStringAsync();
+            
+            using (JsonDocument doc = JsonDocument.Parse(jsonString))
             {
-                var jsonString = await request.Content.ReadAsStringAsync();
-                
-                using (JsonDocument doc = JsonDocument.Parse(jsonString))
+                JsonElement root = doc.RootElement;
+                JsonElement items = root.GetProperty("items");
+                //Console.WriteLine(items.ToString());
+                foreach (JsonElement item in items.EnumerateArray())
                 {
-                    JsonElement root = doc.RootElement;
-                    JsonElement items = root.GetProperty("items");
-                    //Console.WriteLine(items.ToString());
-                    foreach (JsonElement item in items.EnumerateArray())
+                    WebResult webResult = new WebResult();
+                    
+                    webResult.title = item.GetProperty("title").ToString();
+                    webResult.link = item.GetProperty("link").ToString();
+                    webResult.snippet = item.GetProperty("snippet").ToString();
+
+                    if (item.TryGetProperty("pagemap", out JsonElement pagemap))
                     {
-                        WebResult webResult = new WebResult();
-                        
-                        webResult.title = item.GetProperty("title").ToString();
-                        webResult.link = item.GetProperty("link").ToString();
-                        webResult.snippet = item.GetProperty("snippet").ToString();
-
-                        if (item.TryGetProperty("pagemap", out JsonElement pagemap))
+                        //Console.WriteLine(pagemap.ToString());
+                        if (pagemap.TryGetProperty("cse_thumbnail", out JsonElement thumbnail))
                         {
-                            //Console.WriteLine(pagemap.ToString());
-                            if (pagemap.TryGetProperty("cse_thumbnail", out JsonElement thumbnail))
-                            {
-                                var thumnailLink = thumbnail.EnumerateArray().ToArray()[0].GetProperty("src").ToString();
-                                webResult.thumbnailUrl = thumnailLink;
-                            }
+                            var thumnailLink = thumbnail.EnumerateArray().ToArray()[0].GetProperty("src").ToString();
+                            webResult.thumbnailUrl = thumnailLink;
                         }
-                        
-                        //Console.WriteLine(imageObjects.ToString());
-                        
-                        // foreach (JsonElement imageObject in imageObjects.EnumerateArray())
-                        // {
-                        //     webResult.imageLinks.Add(imageObject.GetProperty("contenturl").ToString());
-                        // }
-                        
-                        webResults.Add(webResult);
                     }
-                    Console.WriteLine("Google Geocoding API: Returned Formatted Addresses");
+                    
+                    //Console.WriteLine(imageObjects.ToString());
+                    
+                    // foreach (JsonElement imageObject in imageObjects.EnumerateArray())
+                    // {
+                    //     webResult.imageLinks.Add(imageObject.GetProperty("contenturl").ToString());
+                    // }
+                    
+                    webResults.Add(webResult);
                 }
-
-                return webResults;
-            
-                // Console.WriteLine("Google Custom Search API: Request Successful");
-                //     
-                // Console.WriteLine(jsonString);
+                Console.WriteLine("Google Geocoding API: Returned Formatted Addresses");
             }
-        //}
+
+            return webResults;
+        
+            // Console.WriteLine("Google Custom Search API: Request Successful");
+            //     
+            // Console.WriteLine(jsonString);
+        }
         throw new Exception("Google Custom Search API: Request Failure");
     }
 }
--- ImageContext/Components/Services/OpenAIService.cs
+++ ImageContext/Components/Services/OpenAIService.cs
@@ -15,19 +15,22 @@ public class OpenAIService(IConfiguration configuration)
         string addressToInput = "";
         foreach (string s in originalAddresses)
         {
-            addressToInput += " " + s;
+            addressToInput += "Address: " + s;
+            //Console.WriteLine(addressToInput);
         }
         
         // Take original address and correct it based on the image
         var chat = _api.Chat.CreateConversation();
         chat.RequestParameters.Model = "gpt-4o-mini";
         //chat.RequestParameters.Temperature = 0;
-        chat.RequestParameters.TopP = 0.01;
-        chat.AppendSystemMessage("You will be given an image of a location and a list of address that are near this photo or are in the photo. " +
-                                 "Consider all of these addresses and the image to determine the most accurate address." +
+        chat.RequestParameters.TopP = 0.05;
+        chat.AppendSystemMessage("You will be given an image of a location and a address that is near the location, pictured in the photo, or IS the location pictured in the photo." +
+                                 "If the location, pictured in the photo is different from the given address, then correct the address." +
+                                 "For example: You are given the address \"Stadium Dr & Hagerstown Ln, College Park, MD 20742\", but the location pictured is a stadium. Since Maryland Stadium is near the given address, " +
+                                 "then the correct address would actually be Maryland Stadium, College Park, MD 20742. " +
                                  "Output only the correct address.");
         
-        chat.AppendUserInput(addressToInput, ChatMessage.ImageInput.FromImageBytes(imageBytes, detail:"low"));
+        chat.AppendUserInput(originalAddresses[0], ChatMessage.ImageInput.FromImageBytes(imageBytes, detail:"low"));
         var response = await chat.GetResponseFromChatbotAsync();
         chat.Messages.Clear();
         
@@ -51,6 +54,33 @@ public class OpenAIService(IConfiguration configuration)
         chat.Messages.Clear();
         
         Console.WriteLine("Open AI API: History of Address Successful");
+        
+        return response;
+    }
+    
+    public async Task<string> CreateOptimalSearchTerm(string? address)
+    {
+        // string searchTermString = "";
+        // foreach (string s in searchTerms)
+        // {
+        //     searchTermString += " " + s;
+        // }
+        
+        // Take original address and correct it based on the image
+        var chat = _api.Chat.CreateConversation();
+        chat.RequestParameters.Model = "gpt-4o-mini";
+        //chat.RequestParameters.Temperature = 0;
+        chat.RequestParameters.TopP = 0.01;
+        chat.AppendSystemMessage("You will be given an address, pick the part of the address that would be most ideal as a search term. " +
+                                 "Focus on actual places, if there you aren't given any actual places, focus on the city and region. " +
+                                 "For example, if given Beaver Stadium, University Park, PA 16802. Then you should return: Beaver Stadium. " +
+                                 "But if you were given 1234 North Street, Cleveland, Ohio, 12345. Then you should return: Cleveland Ohio.");
+        
+        chat.AppendUserInput(address);
+        var response = await chat.GetResponseFromChatbotAsync();
+        chat.Messages.Clear();
+        
+        Console.WriteLine("Open AI API: Address Correction Successful");
         
         return response;
     }
